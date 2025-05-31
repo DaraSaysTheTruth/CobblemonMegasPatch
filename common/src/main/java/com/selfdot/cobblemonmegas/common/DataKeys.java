861980a9f474f7c97b29f2ef030970ade38f3d5a
DaraSaysTheTruth <dara.project.officiel@gmail.com>
2025-05-31 16:29:24 +0200

    Added reference to the mega aspect

--- common/src/main/java/com/selfdot/cobblemonmegas/common/DataKeys.java
+++ common/src/main/java/com/selfdot/cobblemonmegas/common/DataKeys.java
@@ -6,8 +6,10 @@ public class DataKeys {
 
     public static final String MOD_NAMESPACE = "cobblemonmegas";
     public static final String MEGA = "mega";
-    public static final String MEGA_X = "mega-x";
+    public static final String MEGA_X = "mega-x"; // referencing to the mega name
     public static final String MEGA_Y = "mega-y";
+    public static final String MEGA_X_ASPECT = "mega_x"; // referencing to the mega aspect
+    public static final String MEGA_Y_ASPECT = "mega_y";
     public static final String NBT_KEY_MEGA_STONE = "megaStone";
     public static final String NBT_KEY_KEY_STONE = "keyStone";
     public static final String NBT_KEY_PREVIOUS_ABILITY = "previousAbility";
